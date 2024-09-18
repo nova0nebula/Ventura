a35e08ce0e64805c9e5614dd891c5fd4b8f2265e
ᑎOᐯᗩ ᑎEᗷᑌᒪᗩ <CosmicNovaNebula@gmail.com>
2024-09-18 18:12:28 +0800

    QuizMaster

--- Constant/Checks/checks.py
+++ Constant/Checks/checks.py
@@ -4,7 +4,6 @@
 
 
 # File Import
-from Projects.Education.QuizMaster.Config.QuizMaster_config import *
 from Constant.Variables.constant_variables import *
 
 
@@ -13,24 +12,6 @@ from Constant.Variables.constant_variables import *
 
 # QuizMaster Checks
 
-def quiz_master_checks():
-    if text_colour in ["white","black","grey","blue","light blue","pink","yellow"]:
-        pass
-    else:
-        print(f"{Fore.RED}Error: text_colour is not within available range.\nError in \033[4mQuizMaster_config.py\033[0m{Fore.RED} on line {text_colour_num}.{Fore.WHITE}")
-        exit()
-
-    if background_colour in ["white","black","grey"]:
-        pass
-    else:
-        print(f"{Fore.RED}Error: background_colour is not within available range.\nError in \033[4mQuizMaster_config.py\033[0m{Fore.RED} on line {background_colour_num}.{Fore.WHITE}")
-        exit()
-
-    if data_file_type in [".pkl",".txt"]:
-        pass
-    else:
-        print(f"{Fore.RED}Error: data_file_type is not within available range.\nError in \033[4mQuizMaster_config.py\033[0m{Fore.RED} on line {data_file_type_num}.{Fore.WHITE}")
-        exit()
 
 
 # End of program
